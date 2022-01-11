61c2567a34d6757afd579a609e2d0d3108e3b27f
naman2807 <namanagarwaldbs438@gmail.com>
2022-01-11 20:07:02 +0530

    Created loadData() function

--- app/src/main/java/com/example/cardvalidation/MainActivity.kt
+++ app/src/main/java/com/example/cardvalidation/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.cardvalidation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.cardvalidation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cardNumber: String
+    private lateinit var dateMonth: String
+    private lateinit var securityCode: String
+    private lateinit var firstName: String
+    private lateinit var lastName: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun loadData(){
+        cardNumber = binding.cardInputLayout.text.toString()
+        dateMonth = binding.dateInputLayout.text.toString()
+        securityCode = binding.cvvInputLayout.text.toString()
+        firstName = binding.firstInputLayout.text.toString()
+        lastName = binding.lastInputLayout.text.toString()
     }
 }
